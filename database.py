68148e9975c28ebb4b4caac1100e52bbc5d91115
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-04-17 15:42:50 -0300

    Restructuring of the file

---
+++ database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+class DataBase:
+    def __init__(self, table_name):
+        self.table_name = table_name
+        self.create_database()
+
+    def create_database(self):
+        db_connection = sqlite3.connect(self.table_name)
+        c = db_connection.cursor()
+
+        c.execute("""CREATE TABLE IF NOT EXISTS witnesses(name TEXT, gender TEXT, pray DATE, read DATE)""")
+        c.execute('''CREATE TABLE IF NOT EXISTS Studient
+                    (name TEXT, gender TEXT, pray DATE, read DATE, first DATE, revisit DATE, course DATE, speech DATE)''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS Ministerial
+                    (name TEXT, gender TEXT, pray DATE, read DATE, treasures DATE, pearls DATE, book DATE, random DATE)''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS Elder
+                    (name TEXT, gender TEXT, pray DATE, read DATE, treasures DATE, pearls DATE, book DATE, random DATE, presidency DATE, needs DATE)''')
+        c.close()
